59063561d8f081f39e6d98b9290ab958519c70e3
Adriano-Java <contato-adriano@live.com>
2021-06-23 15:05:09 -0300

    Domínios iniciais adicionados.

---
+++ src/main/java/br/com/ands/bookstore/domain/Livro.java
@@ -0,0 +1,98 @@
+package br.com.ands.bookstore.domain;
+
+/**
+ * 
+ * @author Adriano Neto Da Silva
+ * @date 23 de jun. de 2021
+ * @project bookstore
+ */
+public class Livro {
+
+	private Integer id;
+	
+	private String titulo;
+	
+	private String nomeDoAutor;
+	
+	private String texto;
+	
+	private Categoria categoria;
+
+	public Livro() {
+		super();
+	}
+
+	public Livro(Integer id, String titulo, String nomeDoAutor, String texto, Categoria categoria) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.nomeDoAutor = nomeDoAutor;
+		this.texto = texto;
+		this.categoria = categoria;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getNomeDoAutor() {
+		return nomeDoAutor;
+	}
+
+	public void setNomeDoAutor(String nomeDoAutor) {
+		this.nomeDoAutor = nomeDoAutor;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Livro other = (Livro) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+} 
