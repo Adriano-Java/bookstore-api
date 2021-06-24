c9b6a7846f496fa8957809d0e84c69ab7936d89f
Adriano-Java <contato-adriano@live.com>
2021-06-24 15:59:46 -0300

    Configurações para o perfil DEV da aplicação.

---
+++ src/main/java/br/com/ands/bookstore/config/DevConfig.java
@@ -0,0 +1,36 @@
+package br.com.ands.bookstore.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import br.com.ands.bookstore.service.DBService;
+
+/**
+ * Configuração para o perfil DEV
+ * 
+ * @author Adriano Neto Da Silva
+ * @date 24 de jun. de 2021
+ * @project bookstore
+ */
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instaciarBancodeDadosH2() {
+		if(strategy.equals("create")) {
+			this.dbService.instanciaBaseDeDados();
+		}
+		return false;
+	}
+
+}
